c57498a22da83a6c31778625b4baaa1430fc630b
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-08-06 16:08:07 -0700

    Adding dotenv config to server and seed.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
